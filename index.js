831097fa45cb6b3de3e60e83ce9b99fd47a5d78d
Alyssa Sinicropi <asinicropi@aim.com>
2016-11-29 19:39:37 -0500

    index

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var restaurants = req.query.restaurants;
 var location = req.query.location;
 
 yelp
-  .search({ term: Gluten Free, location: location })
+  .search({ term: gluten free, location: location })
   .then(function (data){
     res.send(data);
   }).catch(console.error);
